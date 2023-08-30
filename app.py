ea5b12b49cac47cea04800da3b345483e5451f00
Dennis Dachkovski <dennis@dachkovski.de>
2023-08-30 02:27:16 +0200

    aider: Replaced the checkbox with an image display that shows the last uploaded image, which the user can click to reuse.
    # Aider chat conversation:
    
    USER: bei `app.py` in "# Option to reuse the last uploaded image" mache the reuse select not a checkmark but shot the actual last picture, which the user can click.
    
    ASSISTANT: To implement this feature, we need to modify the `app.py` file. We will replace the checkbox with an image display that shows the last uploaded image. The user can click on the image to reuse it.
    
    Here are the steps:
    
    1. We need to store the last uploaded image in a session variable so that it can be accessed across multiple runs of the app. We will add this code right after the image is uploaded and saved.
    
    2. We will replace the checkbox with an image display. If there is an image stored in the session variable, we will display it. The user can click on the image to reuse it.
    
    Here are the proposed changes:
    
    ```python
    app.py
    <<<<<<< HEAD
    # Option to reuse the last uploaded image
    reuse_last_image = st.checkbox("Reuse last uploaded image")
    if reuse_last_image:
        source_url = "uploaded_image.png"
    else:
        # Uploaded image for the talking head
        uploaded_image = st.file_uploader("Upload an image for the talking head", type=["png", "jpg", "jpeg"])
        if uploaded_image is not None:
            with open("uploaded_image.png", "wb") as f:
                f.write(uploaded_image.getbuffer())
            source_url = "uploaded_image.png"
        else:
            source_url = "https://cdn.discordapp.com/attachments/1116787243634397345/1146111608129597450/hypercubefx_face_like_terminator_bb7255e5-efca-489d-bf9e-9aeb750a6bef.png"
    =======
    # Uploaded image for the talking head
    uploaded_image = st.file_uploader("Upload an image for the talking head", type=["png", "jpg", "jpeg"])
    if uploaded_image is not None:
        with open("uploaded_image.png", "wb") as f:
            f.write(uploaded_image.getbuffer())
        source_url = "uploaded_image.png"
        # Store the last uploaded image in a session variable
        st.session_state['last_uploaded_image'] = "uploaded_image.png"
    else:
        # Option to reuse the last uploaded image
        if 'last_uploaded_image' in st.session_state and st.button("Click on the image to reuse it"):
            st.image(st.session_state['last_uploaded_image'])
            source_url = st.session_state['last_uploaded_image']
        else:
            source_url = "https://cdn.discordapp.com/attachments/1116787243634397345/1146111608129597450/hypercubefx_face_like_terminator_bb7255e5-efca-489d-bf9e-9aeb750a6bef.png"
    >>>>>>> updated
    ```
    
    Please note that this change assumes that the last uploaded image is stored in the same directory as the `app.py` file and that the image file is named `uploaded_image.png`. If the image is stored in a different location or has a different name, you will need to adjust the file path and name accordingly.

--- app.py
+++ app.py
@@ -33,17 +33,19 @@ avatar_id = os.getenv("YOUR_AVATAR_ID")  # Assuming this is still from an env va
 D_ID_API_KEY = st.session_state.get('D_ID_API_KEY', '')
 d_id_client = DIdClient(api_key=D_ID_API_KEY)
 
-# Option to reuse the last uploaded image
-reuse_last_image = st.checkbox("Reuse last uploaded image")
-if reuse_last_image:
+# Uploaded image for the talking head
+uploaded_image = st.file_uploader("Upload an image for the talking head", type=["png", "jpg", "jpeg"])
+if uploaded_image is not None:
+    with open("uploaded_image.png", "wb") as f:
+        f.write(uploaded_image.getbuffer())
     source_url = "uploaded_image.png"
+    # Store the last uploaded image in a session variable
+    st.session_state['last_uploaded_image'] = "uploaded_image.png"
 else:
-    # Uploaded image for the talking head
-    uploaded_image = st.file_uploader("Upload an image for the talking head", type=["png", "jpg", "jpeg"])
-    if uploaded_image is not None:
-        with open("uploaded_image.png", "wb") as f:
-            f.write(uploaded_image.getbuffer())
-        source_url = "uploaded_image.png"
+    # Option to reuse the last uploaded image
+    if 'last_uploaded_image' in st.session_state and st.button("Click on the image to reuse it"):
+        st.image(st.session_state['last_uploaded_image'])
+        source_url = st.session_state['last_uploaded_image']
     else:
         source_url = "https://cdn.discordapp.com/attachments/1116787243634397345/1146111608129597450/hypercubefx_face_like_terminator_bb7255e5-efca-489d-bf9e-9aeb750a6bef.png"
 
